dd05c70d91efb02885717fcfc31d85bb8cebf16b
Hemanth Kumar <hemanth.avisi@varthana.com>
2022-01-13 13:33:54 +0530

    Add NameCli

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const clearConsole = require("clear-any-console");
+
+// clear the console
+clearConsole();
+
+console.log(`
+Avisi Hemanth Kumar
+
+Super Good Developer dedicated  to satisfy  end user requirements without compromise.
+`);
