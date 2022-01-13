90b9f9837c7354bd4542f293610feb0e2d9fdb3f
Hemanth Kumar <hemanth.avisi@varthana.com>
2022-01-13 14:04:02 +0530

    Update welcome package

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 #!/usr/bin/env node
 
-const clearConsole = require("clear-any-console");
-
+const pkgJSON = require("./package.json");
+const welcome = require("cli-welcome");
 // clear the console
-clearConsole();
 
+welcome({
+  title: pkgJSON.name,
+  tagLine: "Welcome",
+  description: pkgJSON.description,
+  version: pkgJSON.version,
+  bgColor: "#FADC00",
+  color: "#000000",
+  bold: true,
+  clear: true,
+});
 console.log(`
 Avisi Hemanth Kumar
 
-Super Good Developer dedicated  to satisfy  end user requirements without compromise.
+Super Good Developer dedicated to satisfy  end user requirements without compromise.
 `);
